43d07262f05be1bb1493d6730072be22ce1d120f
NadaHab <91700494+NadaHab@users.noreply.github.com>
2022-09-15 18:56:06 -0400

    second commit

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
 export default function Post() {
   return (
@@ -18,10 +19,20 @@ export default function Post() {
 
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! It's my first post :)</span>
+                <span className="postText">Hey! It's my first post <SentimentSatisfiedAltIcon/>
+                </span>
                 <img src="/assets/post/1.jpg" />
             </div>
-            <div className="postBottom"></div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img className="likeIcon" src="assets/like.png" alt=""/>
+                    <img className="likeIcon" src="assets/heart.png" alt=""/>
+                    <span className="postlikeCounter"> 57 people like it</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="postCommentText"> 12 comments</span>
+                </div>
+            </div>
         </div>
       
     </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -5,7 +5,22 @@ import "./rightbar.css"
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img  className="birthdayImg" src="assets/gift.png" alt=""/>
+          <span className="birthdayTest"><b>Cathy Jones</b>Cathy Jones and <b>2 other friends</b> have a birthday today </span>
+        </div>
+        {/* <img src="assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="assets/person/3.jpg"></img>
+            </div>
+          </li>
+        </ul> */}
+      
+      </div>
     </div>
   )
 }
